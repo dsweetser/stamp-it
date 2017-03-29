7911ec1c8bada5e32f71fc04d342d912db73ead3
dsweetser <daniel.sweetser@gmail.com>
2017-03-29 10:55:39 -0400

    Add complete order button
    Created complete order component button app-order/confirm
    Added complete order button to shop/orders if order is not completed
    Feature complete
    Next will be to start defining the stamp resource

--- app/routes/shop/orders.js
+++ app/routes/shop/orders.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model () {
     return this.get('store').findAll('order');
   },
+
+  actions: {
+    confirmOrder (order) {
+      order.toggleProperty('paid');
+      order.save()
+      .then(() => this.transitionTo('shop.orders'));
+    },
+  }
 });
