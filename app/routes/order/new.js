474c68bb40a977378d1565c597e5319dfa3b3d0a
dsweetser <daniel.sweetser@gmail.com>
2017-03-30 11:40:57 -0400

    Remove code from old route
    This is an outdate route - keeping it around in case I need it later,
      but removed code to avoid confusion

--- app/routes/order/new.js
+++ app/routes/order/new.js
@@ -1,17 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model () {
-    return this.get('store').createRecord('order', {});
-  },
-
-  actions: {
-    createOrder(order){
-      order.save()
-      .then(() => this.transitionTo('shop'));
-    },
-    cancel () {
-      history.back();
-    },
-  },
 });
